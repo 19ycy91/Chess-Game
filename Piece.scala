d33627b2ba768f5a1a1c836e46d6154e58b7665b
richardyoon91 <richardyoon91@gmail.com>
2016-06-23 13:23:22 -0400

    Update Piece.scala

--- Piece.scala
+++ Piece.scala
@@ -6,4 +6,19 @@ abstract class Piece(val player:Player){
   }
   
 class p(player:Player) extends Piece(player){
+  def allowedmoves(Position:(Int,Int))={
+    if (player.color == "W"){
+      if (y>0){
+        Array((x,y-1))
+      }
+      else return Array()
+    }
+    else{
+      if (y<7){
+        return Array(x,y+1)
+      }
+      else return Array()
+    }
+  }
+  }
   
