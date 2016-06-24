1056eacf21d67c90bcca4c8644aa2a44a6748c9a
richardyoon91 <richardyoon91@gmail.com>
2016-06-23 21:33:10 -0400

    Update Piece.scala

--- Piece.scala
+++ Piece.scala
@@ -1,24 +1,47 @@
-//piece
+package Chess
+import scala.math._
+/**
+  * Created by Richard Yoon on 6/22/2016.
+  */
+abstract class Piece(val player:Player) {
+  def allowedMoves(Positions:(Int,Int)):List[(Int,Int)]
+  def pathtaken(Position:(Int,Int),NewPosition:(Int,Int)):List[(Int,Int)]
 
 
-abstract class Piece(val player:Player){
-  def allowedMoves;
-  }
-  
+}
+
+//types
+//p r n b q k
+
+//position will be stored in the board
+//dummy class pawn that only goes up and down
 class p(player:Player) extends Piece(player){
-  def allowedMoves(Position:(Int,Int))={
-    if (player.color == "W"){
-      if (y>0){
-        Array((x,y-1))
+  override def allowedMoves(Position:(Int,Int)):List[(Int,Int)]={
+    if (player.Color =="W"){
+      if ( Position._2 >0){
+        List((Position._1,Position._2-1))
       }
-      else return Array()
+      else List()
     }
     else{
-      if (y<7){
-        return Array(x,y+1)
+      if (Position._2<7){
+        List((Position._1,Position._2+1))
       }
-      else return Array()
+      else List()
     }
   }
+  override def pathtaken(Position:(Int,Int),NewPosition:(Int,Int)):List[(Int,Int)]={
+    val step:Int = (NewPosition._2 - Position._2)
+    val step1:Int = step/abs(step)
+    var PathTaken = List[(Int,Int)]()
+    var movespot = Position._2
+    while (NewPosition._2!=movespot){
+      movespot+=step1
+      PathTaken::=((Position._1,movespot))
+    }
+    return PathTaken
   }
-  
+  }
+
+
+
